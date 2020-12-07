3d2a5ec814e79f0f03893f67fffa7ee55d192a31
jordanstrobing <jordanstrobing@192.168.1.10>
2020-12-07 17:01:13 -0500

    First commit

---
+++ multiplicationtable/src/multiplicationtable.java
@@ -0,0 +1,19 @@
+
+public class multiplicationtable {
+	
+	public static void main (String []args) {
+		for(int i = 1; i <= 10; i++) {
+			for(int j = 1; j <= 10; j++) {
+				int num = i * j;
+				System.out.printf(num + "\t");
+			}
+			System.out.println();
+		}
+		
+		
+		
+		
+	}
+		
+		
+}
